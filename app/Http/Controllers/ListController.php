ee0992d64896b4c565b5797cbd141b06cb9a65d5
trong874 <pvt874@gmail.com>
2021-08-05 11:39:07 +0700

    move card backend

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -51,21 +51,24 @@ class ListController extends Controller
 
     public function showListByBoardId($board_id): \Illuminate\Http\JsonResponse
     {
-        $lists = ListModel::where('board_id', $board_id)->orderBy('location')->get();
+        $lists = ListModel::with('cards')
+            ->orderBy('location')
+            ->where('board_id',$board_id)
+            ->get();
         return response()->json([
-            'list' => $lists
+            'lists' => $lists,
         ]);
     }
 
     public function moveList(Request $request)
     {
-        $location = $request->location;
-        $listId = $request->listId;
-
-        $list = ListModel::find($listId);
-        $list->location = $location;
-        $list->save();
-        return response()->json("abc");
+        $data = $request->all();
+        foreach ($data as $key => $list){
+            $list = ListModel::find($list['id']);
+            $list->location = $key;
+            $list->save();
+        }
+        return response()->json($data);
     }
 
     public function changeTitle(Request $request): \Illuminate\Http\JsonResponse
